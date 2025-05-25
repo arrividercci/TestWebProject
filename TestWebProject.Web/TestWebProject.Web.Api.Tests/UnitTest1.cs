1cc8efd8aa70b4f05ff74b59e6a92176ed2f8628
Влад Кучинський <vchelovek0@gmail.com>
2025-05-25 18:18:49 +0300

    created api and tests projects

---
+++ TestWebProject.Web/TestWebProject.Web.Api.Tests/UnitTest1.cs
@@ -0,0 +1,27 @@
+namespace TestWebProject.Web.Api.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+            Assert.Equal(1, 1);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+            Assert.Equal(true, true);
+        }
+    }
+}
